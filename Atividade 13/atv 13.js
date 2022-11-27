61f04a44a56d6ba0abfb9091cafbcfbc3dc59c43
marcioadsc <78886285+marcioadsc@users.noreply.github.com>
2022-11-27 15:57:31 -0300

    Add files via upload

---
+++ Atividade 13/atv 13.js
@@ -0,0 +1,12 @@
+const toUpper = () =>{
+      const input = document.body.querySelector('#inputText');
+      input.value = input.value.toUpperCase();
+};
+
+const toLower = () =>{
+      const input = document.body.querySelector('#inputText');
+      input.value = input.value.toLowerCase();
+};
+
+
+
